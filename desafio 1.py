ec818457d7940e8e67331afc529bc48cccceb407
Faby24 <faby240192@gmail.com>
2025-04-03 22:03:00 -0300

    Add files via upload

---
+++ desafio 1.py
@@ -0,0 +1,10 @@
+# Definir dos números
+numero1 = 8  
+numero2 = 6  
+
+# Sumar los números
+resultado = numero1 + numero2
+
+# Mostrar el resultado
+print(f"La suma de {numero1} y {numero2} es: {resultado}")
+
---
+++ desafio 2.py
@@ -0,0 +1,12 @@
+# Definir las dimensiones del rectángulo
+ancho = 4  
+largo = 6 
+# Definir texto explicativo
+texto = "El área de un rectángulo se calcula multiplicando el ancho por el largo."
+
+# Calcular el área
+area = ancho * largo
+
+# Mostrar el resultado
+print(texto)
+print(f"El área del rectángulo con un ancho de {ancho} y un largo de {largo} es: {area}")
